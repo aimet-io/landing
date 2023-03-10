aaf6b6187987b6894c018539375ec5c8a708cb72
Vicenzo <arturo97xd@gmail.com>
2023-03-09 21:59:31 -0500

    about_es_en

--- views/About/index.jsx
+++ views/About/index.jsx
@@ -10,181 +10,170 @@ import Spacing from "@/components/Spacing";
 import { useTranslation } from "next-i18next";
 
 export function AboutView() {
-  const { t } = useTranslation("about");
+const { t } = useTranslation("about");
 
-  pageTitle("About");
+pageTitle("About");
 
-  const funfaceData = [
-    {
-      title: "Global Happy Clients",
-      factNumber: "40K",
-    },
-    {
-      title: "Project Completed",
-      factNumber: "50K",
-    },
-    {
-      title: "Team Members",
-      factNumber: "245",
-    },
-    {
-      title: "Digital products",
-      factNumber: "550",
-    },
-  ];
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <>
-      {/* Start Page Heading Section */}
-      <PageHeading
-        title={t("heading.title")}
-        bgSrc="images/about_hero_bg.jpeg"
-        pageLinkText="About Us"
+const funfactData = [
+  {
+    title: t("funfactData.title1"),
+    factNumber: "40K",
+  },
+  {
+    title: t("funfactData.title2"),
+    factNumber: "50K",
+  },
+  {
+    title: t("funfactData.title3"),
+    factNumber: "245",
+  },
+  {
+    title: t("funfactData.title4"),
+    factNumber: "550",
+  },
+];
+useEffect(() => {
+  window.scrollTo(0, 0);
+}, []);
+return (
+  <>
+    {/* Start Page Heading Section */}
+    <PageHeading
+      title={t("heading.title")}
+      bgSrc="images/about_hero_bg.jpeg"
+      pageLinkText="About Us"
+    />
+    {/* End Page Heading Section */}
+
+    {/* Start About Section */}
+    <Spacing lg="150" md="80" />
+    <Div className="container">
+      <Div className="row">
+        <Div className="col-xl-5 col-lg-7">
+          <SectionHeading
+            title={t("aboutHeading.title")}
+            subtitle={t("aboutHeading.subtitle")}
+          >
+            <Spacing lg="30" md="20" />
+            <p className="cs-m0">
+              {t("aboutHeading.paragraph")}
+            </p>
+            <Spacing lg="30" md="30" />
+            <Div className="cs-separator cs-accent_bg"></Div>
+            <Spacing lg="25" md="40" />
+          </SectionHeading>
+        </Div>
+        <Div className="col-lg-5 offset-xl-2">
+          <img
+            src="/images/about_img_1.jpeg"
+            alt="About"
+            className="w-100 cs-radius_15"
+          />
+          <Spacing lg="25" md="25" />
+        </Div>
+        <Div className="col-lg-7">
+          <img
+            src="/images/about_img_2.jpeg"
+            alt="About"
+            className="w-100 cs-radius_15"
+          />
+          <Spacing lg="25" md="25" />
+        </Div>
+        <Div className="col-lg-5">
+          <img
+            src="/images/about_img_3.jpeg"
+            alt="About"
+            className="w-100 cs-radius_15"
+          />
+          <Spacing lg="25" md="25" />
+        </Div>
+      </Div>
+    </Div>
+    <Spacing lg="75" md="55" />
+    {/* End About Section */}
+
+    {/* Start Fun Fact Section */}
+    <Div className="container">
+      <FunFact
+        title={t("funfact.title")}
+        subtitle={t("funfact.subtitle")}
+        data={funfactData}
       />
-      {/* End Page Heading Section */}
+    </Div>
+    {/* End Fun Fact Section */}
 
-      {/* Start About Section */}
-      <Spacing lg="150" md="80" />
-      <Div className="container">
-        <Div className="row">
-          <Div className="col-xl-5 col-lg-7">
-            <SectionHeading
-              title="Your trusted partner for business"
-              subtitle="About Our Agency"
-            >
-              <Spacing lg="30" md="20" />
-              <p className="cs-m0">
-                This is the main factor that sets us apart from our competition
-                and allows us to deliver a specialist business consultancy
-                service. Our team applies its wide-ranging experience to
-                determining. Through our years of experience, we’ve also learned
-                that while each channel.
-              </p>
-              <Spacing lg="30" md="30" />
-              <Div className="cs-separator cs-accent_bg"></Div>
-              <Spacing lg="25" md="40" />
-            </SectionHeading>
-          </Div>
-          <Div className="col-lg-5 offset-xl-2">
-            <img
-              src="/images/about_img_1.jpeg"
-              alt="About"
-              className="w-100 cs-radius_15"
-            />
-            <Spacing lg="25" md="25" />
-          </Div>
-          <Div className="col-lg-7">
-            <img
-              src="/images/about_img_2.jpeg"
-              alt="About"
-              className="w-100 cs-radius_15"
-            />
-            <Spacing lg="25" md="25" />
-          </Div>
-          <Div className="col-lg-5">
-            <img
-              src="/images/about_img_3.jpeg"
-              alt="About"
-              className="w-100 cs-radius_15"
-            />
-            <Spacing lg="25" md="25" />
-          </Div>
-        </Div>
-      </Div>
-      <Spacing lg="75" md="55" />
-      {/* End About Section */}
-
-      {/* Start Fun Fact Section */}
-      <Div className="container">
-        <FunFact
-          title="Our fun fact"
-          subtitle="Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
-          data={funfaceData}
-        />
-      </Div>
-      {/* End Fun Fact Section */}
-
-      {/* Start Why Choose Section */}
-      <Spacing lg="100" md="80" />
-      <Div className="container">
-        <Div className="row">
-          <Div className="col-xl-5 col-lg-6">
-            <Div className="cs-image_layer cs-style1">
-              <Div className="cs-image_layer_in">
-                <img
-                  src="/images/about_img_4.jpeg"
-                  alt="About"
-                  className="w-100 cs-radius_15"
-                />
-              </Div>
+    {/* Start Why Choose Section */}
+    <Spacing lg="100" md="80" />
+    <Div className="container">
+      <Div className="row">
+        <Div className="col-xl-5 col-lg-6">
+          <Div className="cs-image_layer cs-style1">
+            <Div className="cs-image_layer_in">
+              <img
+                src="/images/about_img_4.jpeg"
+                alt="About"
+                className="w-100 cs-radius_15"
+              />
             </Div>
-            <Spacing lg="0" md="40" />
           </Div>
-          <Div className="col-xl-5 offset-xl-1 col-lg-6">
-            <SectionHeading
-              title="Highly experienced pepole with us"
-              subtitle="Why Choose Us"
-            >
-              <Spacing lg="30" md="20" />
-              <p className="cs-m0">
-                This is the main factor that sets us apart from our competition
-                and allows us to deliver a specialist business consultancy
-                service. Our team applies its wide-ranging experience to
-                determining. Through our years of experience, we’ve also learned
-                that while each channel.
-              </p>
-              <Spacing lg="15" md="15" />
-              <p className="cs-m0">
-                This is the main factor that sets us apart from our competition
-                and allows us to deliver a specialist business consultancy
-                service. Our team applies its wide-ranging experience to
-                determining.
-              </p>
-              <Spacing lg="30" md="30" />
-              <Div className="cs-separator cs-accent_bg"></Div>
-              <Spacing lg="25" md="0" />
-            </SectionHeading>
-          </Div>
+          <Spacing lg="0" md="40" />
+        </Div>
+        <Div className="col-xl-5 offset-xl-1 col-lg-6">
+          <SectionHeading
+            title={t("whyChooseUs.title")}
+            subtitle={t("whyChooseUs.subtitle")}
+          >
+            <Spacing lg="30" md="20" />
+            <p className="cs-m0">
+              {t("whyChooseUs.paragraph")}
+            </p>
+            <Spacing lg="15" md="15" />
+            <p className="cs-m0">
+            {t("whyChooseUs.paragraph2")}
+            </p>
+            <Spacing lg="30" md="30" />
+            <Div className="cs-separator cs-accent_bg"></Div>
+            <Spacing lg="25" md="0" />
+          </SectionHeading>
         </Div>
       </Div>
-      {/* End Why Choose Section */}
+    </Div>
+    {/* End Why Choose Section */}
 
-      {/* Start Team Section */}
-      <Spacing lg="145" md="80" />
-      <Div className="container">
-        <SectionHeading
-          title={
-            <>
-              Awesome team <br />
-              members
-            </>
-          }
-          subtitle="Our Team"
-          variant="cs-style1"
-        />
-        <Spacing lg="85" md="45" />
-        <TeamSlider />
-      </Div>
-      {/* End Team Section */}
+    {/* Start Team Section */}
+    <Spacing lg="145" md="80" />
+    <Div className="container">
+      <SectionHeading
+        title={
+          <>
+            {t("team.title")} <br />
+            {t("team.title2")}
+          </>
+        }
+        subtitle={t("team.subtitle")}
+        variant="cs-style1"
+      />
+      <Spacing lg="85" md="45" />
+      <TeamSlider />
+    </Div>
+    {/* End Team Section */}
 
-      {/* Start CTA Section */}
-      <Spacing lg="150" md="80" />
-      <Div className="container">
-        <Cta
-          title={
-            <>
-              Let’s disscuse make <br />
-              something <i>cool</i> together
-            </>
-          }
-          btnText="Apply For Meeting"
-          btnLink="/contact"
-          bgSrc="/images/cta_bg.jpeg"
-        />
-      </Div>
-      {/* End CTA Section */}
-    </>
-  );
+    {/* Start CTA Section */}
+    <Spacing lg="150" md="80" />
+    <Div className="container">
+      <Cta
+        title={
+          <>
+            {t("cta.title")} <br />
+            {t("cta.title2")} <i>{t("cta.title3")}</i> {t("cta.title4")}
+          </>
+        }
+        btnText={t("cta.btnText")}
+        btnLink="/contact"
+        bgSrc="/images/cta_bg.jpeg"
+      />
+    </Div>
+    {/* End CTA Section */}
+  </>
+);
 }
