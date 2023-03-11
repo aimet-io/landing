0dba85218fa72830deb37a54ae9781b5ef344345
darwinva97 <darwin97.va@hotmail.com>
2023-03-10 19:27:33 -0500

    title in About page is translated

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,18 @@
 import { AboutView } from "@/views/About";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import Head from "next/head";
 
 const AboutPage = () => {
-  return <AboutView />;
+  const { t } = useTranslation("about")
+  return (
+    <>
+      <Head>
+        <title>{t("metaTitle")}</title>
+      </Head>
+      <AboutView />
+    </>
+  )
 };
 
 export async function getStaticProps({ locale }) {
