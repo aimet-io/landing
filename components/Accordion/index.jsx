fb5031f1fd43b1b819e86a79e10cc8a71d086e62
rafael199610222 <rmallqui@fi.uba.ar>
2023-03-13 13:58:26 -0300

    traduccion faq

--- components/Accordion/index.jsx
+++ components/Accordion/index.jsx
@@ -1,54 +1,68 @@
-import React from 'react'
-import { useState } from 'react'
-import Div from '../Div'
-
+import React from "react";
+import { useState } from "react";
+import Div from "../Div";
+import { useTranslation } from "next-i18next";
 export default function Accordion() {
-  const [selected, setSelected] = useState(0)
+  const { t } = useTranslation("faq");
+
+  const [selected, setSelected] = useState(0);
   const handelToggle = (index) => {
-    if(selected === index) {
-      return setSelected(null)
+    if (selected === index) {
+      return setSelected(null);
     }
-    setSelected(index)
-  }
+    setSelected(index);
+  };
   const accordionData = [
     {
-      question: 'Do you design illustration website?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: t("preguntas.pregunta1"),
+      answer: t("preguntas.respuesta1"),
     },
     {
-      question: 'Do you provide design source file after finish work?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: t("preguntas.pregunta2"),
+      answer: t("preguntas.respuesta2"),
     },
     {
-      question: 'How to provide project details and payments?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: t("preguntas.pregunta3"),
+      answer: t("preguntas.respuesta3"),
     },
     {
-      question: 'Can you tell me please how to contact for project?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: t("preguntas.pregunta4"),
+      answer: t("preguntas.respuesta4"),
     },
     {
-      question: 'Do you makes custom logo, icon etc?',
-      answer: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesent voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
+      question: t("preguntas.pregunta5"),
+      answer: t("preguntas.respuesta5"),
     },
-  ]
+  ];
   return (
     <Div className="cs-accordians cs-style1">
-      {accordionData.map((item, index)=>(
-        <Div className={`cs-accordian ${selected===index?'active':''}`} key={index}>
-          <Div className="cs-accordian_head" onClick={()=>handelToggle(index)}>
+      {accordionData.map((item, index) => (
+        <Div
+          className={`cs-accordian ${selected === index ? "active" : ""}`}
+          key={index}
+        >
+          <Div
+            className="cs-accordian_head"
+            onClick={() => handelToggle(index)}
+          >
             <h2 className="cs-accordian_title">{item.question}</h2>
             <span className="cs-accordian_toggle cs-accent_color">
-              <svg width={15} height={8} viewBox="0 0 15 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                width={15}
+                height={8}
+                viewBox="0 0 15 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M0 0L7.5 7.5L15 0H0Z" fill="currentColor" />
-              </svg>                    
+              </svg>
             </span>
           </Div>
-          <Div className='cs-accordian_body'>
+          <Div className="cs-accordian_body">
             <Div className="cs-accordian_body_in">{item.answer}</Div>
-            </Div>
+          </Div>
         </Div>
       ))}
     </Div>
-  )
+  );
 }
